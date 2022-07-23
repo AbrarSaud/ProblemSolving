6db1a39a7c7e42b6fb4e34293e555e436576a2ae
AbrarSaud <109272922+AbrarSaud@users.noreply.github.com>
2022-07-23 14:27:29 +0300

    Add files via upload

---
+++ SaudiCPC1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+
+public class SaudiCPC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner myObj = new Scanner(System.in);
+		
+	//	int t = myObj.nextInt();
+		
+		
+		//for(int i =0; i< 4 ; i++) {
+		
+			int a = myObj.nextInt();
+			int b = myObj.nextInt();
+			int c = myObj.nextInt();
+			int d = myObj.nextInt();
+
+			System.out.println(a * b * c * d);
+			
+		//}
+		}}
+	
+	
+
+		
+	
+
+
