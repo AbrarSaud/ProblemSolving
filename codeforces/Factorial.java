ca4cb4d79da617de06038b4b4f60d6feb181196f
AbrarSaud <109272922+AbrarSaud@users.noreply.github.com>
2022-09-06 23:11:44 +0300

    Add files via upload

---
+++ codeforces/Factorial.java
@@ -0,0 +1,19 @@
+// B. Factorial
+// https://codeforces.com/group/ndZ5Fwlwj8/contest/312340/problem/B
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) {
+		try (// TODO Auto-generated method stub
+				Scanner myObj = new Scanner(System.in)) {
+			long num = myObj.nextLong();
+			
+			if(num == 1) {
+				System.out.println("NO");
+			}else {
+				System.out.println("YES");
+			}
+		}
+	}
+
+}
